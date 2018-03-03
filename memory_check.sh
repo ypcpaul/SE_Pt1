ade9b0d8b1e29eccab2795126b9bfe444afaed56
Paul Cayaco <ypaulc@gmail.com>
2018-03-03 13:40:07 +0800

    Checking of used memory against critical and warning thresholds.  Also removed debugging echo statements

--- memory_check.sh
+++ memory_check.sh
@@ -10,15 +10,12 @@ while getopts :c:w:e: opt; do
   case $opt in
     c)
       CRITICAL_THRESHOLD=${OPTARG}
-      echo "-c param: ${OPTARG}"
       ;;
     w)
       WARNING_THRESHOLD=${OPTARG}
-      echo "-w param: ${OPTARG}"
       ;;
     e)
       EMAIL=${OPTARG}
-      echo "-e param: ${OPTARG}"
       ;;
   esac
 done
@@ -27,4 +24,24 @@ done
 if [[ -z ${CRITICAL_THRESHOLD+x} || -z ${WARNING_THRESHOLD+x} || -z ${EMAIL+x} || ${CRITICAL_THRESHOLD} -lt ${WARNING_THRESHOLD} ]]; then
   echo 'The following parameters are required: -c (critical threshold %) -w (warning threshold %)  -e (email address).  Critical threshold must always be greter than warning threshold.'
   exit
+fi
+
+USED_MEMORY=$( free | grep Mem: | awk '{ print $3}' )
+TOTAL_MEMORY=$( free | grep Mem: | awk '{ print $2}' )
+
+# use perl, since bash only deals with integers
+USED_PERCENTAGE=$(perl -e "printf('%.0f', ${USED_MEMORY}/${TOTAL_MEMORY}*100)")
+
+echo $USED_PERCENTAGE
+
+if [[ $USED_PERCENTAGE -ge $CRITICAL_THRESHOLD ]]; then
+  exit 2
+fi
+
+if [[ $USED_PERCENTAGE -ge $WARNING_THRESHOLD ]]; then
+  exit 1
+fi
+
+if [[ $USED_PERCENTAGE -lt $WARNING_THRESHOLD ]]; then
+  exit 0
 fi
